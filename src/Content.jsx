db45d2ca7cae087f03729699953659493250cb72
Arroneous2 <thumpinbassbeat@gmail.com>
2024-06-13 12:44:45 -0500

    show basic rentals index

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,24 @@
+import { RentalsIndex } from "./RentalsIndex";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 export function Content() {
+  const [rentals, setRentals] = useState([]);
+
+  const handleRentals = () => {
+    console.log("handleRentals");
+    axios.get("http://localhost:3000/rentals.json").then((response) => {
+      console.log(response.data);
+      setRentals(response.data);
+    });
+  };
+
+  useEffect(handleRentals, []);
+
   return (
     <main>
       <h1>Welcome to React!</h1>
+      <RentalsIndex rentals={rentals} />
     </main>
   );
 }
---
+++ src/RentalsIndex.jsx
@@ -0,0 +1,23 @@
+export function RentalsIndex(props) {
+  return (
+    <div>
+      <h1>All the photos</h1>
+      {props.rentals.map((rental) => (
+        <div key={rental.id}>
+          <h1>{rental.name}</h1>
+          <p>User id:{rental.user_id}</p>
+          <p>Address: {rental.address}</p>
+          <p>city: {rental.city}</p>
+          <p>state: {rental.state}</p>
+          <p>price: {rental.price}</p>
+          <p>description: {rental.description}</p>
+          <p>home_type: {rental.home_type}</p>
+          <p>rental_type: {rental.rental_type}</p>
+          <p>total_occupancy: {rental.total_occupancy}</p>
+          <p>total_bed_rentals: {rental.total_bed_rentals}</p>
+          <p>total_bathrooms: {rental.total_bathrooms}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
